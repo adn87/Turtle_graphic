789676ddef23e496997440704a1cd9af2f60d10e
adn87 <aaghadieh@gmail.com>
2023-10-17 15:00:17 -0400

    Turtle_graphic_02
    - code clean up

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("red")
 
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
 
 
 
