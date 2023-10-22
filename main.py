231355b0f9d5a67aa7a7063e30abdf98bee7e0e4
adn87 <aaghadieh@gmail.com>
2023-10-22 13:58:24 -0400

    Turtle_graphic_03
    - created functions to draw a spirograph using the turtle module

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-from turtle import Turtle, Screen
+import turtle as t
+import random
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-
-for _ in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+t.colormode(255)
+tim = t.Turtle()
+tim.shape("turtle")
 
 
+def random_color():
+    r = random.randint(0, 255)
+    b = random.randint(0, 255)
+    g = random.randint(0, 255)
+    color = (r, b, g)
+    return color
 
 
+tim.speed(0)
 
-screen = Screen()
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.circle(100)
+        tim.color(random_color())
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+draw_spirograph(5)
+screen = t.Screen()
 screen.exitonclick()
