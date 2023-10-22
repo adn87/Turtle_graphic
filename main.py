1d4069b3171e9ddb7bf23d525ca079c704feee62
adn87 <aaghadieh@gmail.com>
2023-10-22 18:56:08 -0400

    Turtle_graphic_the_hirst_painting_projects_01
    - added an image of the Hirst painting
    - extracted the colors from the image

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
-import turtle as t
-import random
+import colorgram
 
-t.colormode(255)
-tim = t.Turtle()
-tim.shape("turtle")
+# extracting the color from the img
+# rgb_colors = []
+# colors = colorgram.extract('img.png', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+# print(rgb_colors)
 
+color_list = [(204, 164, 107), (151, 73, 47), (239, 245, 240), (235, 237, 243), (52, 93, 124), (223, 201, 136), (169, 153, 41),
+ (136, 32, 22), (132, 162, 185), (201, 91, 70), (48, 122, 88), (67, 47, 41), (14, 100, 73), (147, 178, 146),
+ (162, 142, 157), (234, 175, 165), (111, 74, 77), (18, 85, 89), (183, 205, 172), (55, 45, 48), (154, 16, 20),
+ (39, 61, 74), (49, 65, 79), (83, 146, 128), (178, 89, 93), (179, 191, 207), (110, 127, 150), (215, 177, 182)]
 
-def random_color():
-    r = random.randint(0, 255)
-    b = random.randint(0, 255)
-    g = random.randint(0, 255)
-    color = (r, b, g)
-    return color
-
-
-tim.speed(0)
-
-
-def draw_spirograph(size_of_gap):
-    for _ in range(int(360 / size_of_gap)):
-        tim.circle(100)
-        tim.color(random_color())
-        tim.setheading(tim.heading() + size_of_gap)
-
-
-draw_spirograph(5)
-screen = t.Screen()
-screen.exitonclick()
